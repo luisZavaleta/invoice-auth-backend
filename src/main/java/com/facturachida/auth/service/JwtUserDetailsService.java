60570ea0e23cb972a97560dd813df9db1f464e9c
Luis Zavaleta <luiszavaleta@Luiss-MacBook-Air.local>
2019-08-07 15:44:09 -0500

    Get user from DB

--- src/main/java/com/facturachida/auth/service/JwtUserDetailsService.java
+++ src/main/java/com/facturachida/auth/service/JwtUserDetailsService.java
@@ -31,8 +31,10 @@ public class JwtUserDetailsService implements UserDetailsService {
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 
-		if ("luis".equals(username)) {
-			return new User("luis", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+		AuthUser aUser = userRepository.findByUserName(username);
+
+		if (aUser != null){
+			return new User(aUser.getUsername(), aUser.getPassword(),
 					new ArrayList<>());
 		} else {
 			throw new UsernameNotFoundException("User not found with username: " + username);
