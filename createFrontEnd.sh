8df93d6243d7fd05b94dd90111c3c09ee1771e82
Luis Zavaleta <luiszavaleta@Luiss-MacBook-Air.local>
2019-09-10 13:30:10 -0500

    delete that

--- createFrontEnd.sh
+++
@@ -1,3 +0,0 @@
-./gradlew build
-docker build -t auth-app:latest --build-arg JAR_FILE=./build/libs/auth-0.0.1-SNAPSHOT.jar .
-docker run --network factura-login-network  --name auth-app auth-app:latest
